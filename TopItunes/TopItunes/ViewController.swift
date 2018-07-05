32d7405475050bc37f6d875f382a5c2d95b53e3e
Roshy <Roshy_s7@icloud.com>
2018-07-05 22:32:51 +0300

    get request
    get names from internet

--- TopItunes/TopItunes/ViewController.swift
+++ TopItunes/TopItunes/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+     //connect to model
+ fileprivate var  listOfMove = [Moves]()
+    
+    @IBOutlet weak var topMovesCollection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        //request
+        let apiToContact = "https://rss.itunes.apple.com/api/v1/us/movies/top-movies/all/10/explicit.json"
+        
+        Alamofire.request(apiToContact, method: .get).responseJSON { response in
+            guard response.result.isSuccess else {
+                print("Error request \(String(describing: response.result.error))")
+                
+                return
+            }
+            if  let arrayOfItems = response.result.value {
+                let json = JSON(arrayOfItems)
+                let topMovieDataArray = json["feed"]["results"].arrayValue
+               //print(topMovieDataArray)
+                
+                for item in topMovieDataArray {
+                    let move = Moves(json: item )
+                    self.listOfMove.append(move)
+                    
+                }
+            }
+            DispatchQueue.main.async {
+                self.topMovesCollection.reloadData()
+            }
+            
+        }
+       
+        
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,13 +59,15 @@ class ViewController: UIViewController {
 }
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return listOfMove.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
          let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MoveCell", for: indexPath) as! MoveCell
-        cell.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-        cell.moveName.text = "New Move"
+       cell.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        //cell.moveName.text = "New Move"
+         let textData = listOfMove[indexPath.row]
+        cell.moveName.text = textData.name
         return cell
     }
     
